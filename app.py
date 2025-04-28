7456d509a6667696a4101a5365535982a5f13dfc
beckynayere <rebecca.nayere@gmail.com>
2025-04-28 16:39:33 +0300

    remove requirements

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+model = joblib.load('model.pkl')
+
+@app.route('/')
+def home():
+    return "🌸 Iris Flower Predictor is Live!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = model.predict([data['features']])
+    return jsonify({
+        'predicted_class': int(prediction[0])
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
